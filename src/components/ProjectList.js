4fe43521edc59bca4243e39443a994fe63b80204
Valeria Ze <valerijazhe@gmail.com>
2024-05-19 19:27:58 +0300

    projectlist

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 const ProjectList = ({ subcategoryId, categoryId }) => {
   const [projects, setProjects] = useState([]);
-
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -17,33 +16,41 @@ const ProjectList = ({ subcategoryId, categoryId }) => {
           url += `/category/${categoryId}/all`;
         }
         const response = await axios.get(url);
+        console.log('Response data:', response.data); // Отладочная информация
         setProjects(response.data);
       } catch (error) {
         console.error('Error fetching projects:', error);
+        setProjects([]); // Установите пустой массив в случае ошибки
       }
     };
 
     fetchProjects();
   }, [subcategoryId, categoryId]);
 
+  // Дополнительные проверки для отладки
+  if (!Array.isArray(projects)) {
+    console.error('Projects is not an array:', projects);
+    return <div>Error: Projects data is invalid.</div>;
+  }
+
   return (
     <div>
       {projects.map(project => (
-        <div class="project-block" key={project.project_id}>
+        <div className="project-block" key={project.project_id}>
           <Row>
-            <Col style={{padding:'0'}}>
-              <img src={`/images/${project.image_name}`} alt={project.title} style={{maxWidth:'550px', height:'400px'}} />
+            <Col style={{ padding: '0' }}>
+              <img src={`/images/${project.image_name}`} alt={project.title} style={{ maxWidth: '550px', height: '400px' }} />
             </Col>
-            <Col style={{padding:'50px', backgroundColor:'rgba(107, 174, 255, 0.2)'}}>
-              <h3 style={{color:"white"}}>{project.title}</h3>
-              <p class="white-text" style={{fontSize:'20px'}}>{project.subtitle}</p>
-              <p class="white-text">{project.description}</p>
+            <Col style={{ padding: '50px', backgroundColor: 'rgba(107, 174, 255, 0.2)' }}>
+              <h3 style={{ color: "white" }}>{project.title}</h3>
+              <p className="white-text" style={{ fontSize: '20px' }}>{project.subtitle}</p>
+              <p className="white-text">{project.description}</p>
               <p className="white-text">{project.category.name} | {project.subcategory.name}</p>
 
               <Link to={`/project/${project.project_id}`}>
-                <button class="arrow-buttonWhite">
-                СМОТРЕТЬ ПРОЕКТ
-                <img class="icon-arrow" src="images/arrowWhite.svg"></img>
+                <button className="arrow-buttonWhite">
+                  СМОТРЕТЬ ПРОЕКТ
+                  <img className="icon-arrow" src="images/arrowWhite.svg" alt="arrow icon" />
                 </button>
               </Link>
             </Col>
