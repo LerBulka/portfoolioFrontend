04b9ef6b8171346e5bf80a5c05d5dfb06d2f215b
Valeria Ze <valerijazhe@gmail.com>
2024-05-19 19:34:27 +0300

    subcategories

--- src/components/SubcategoryTabs.js
+++ src/components/SubcategoryTabs.js
@@ -10,17 +10,31 @@ const SubcategoryTabs = ({ categoryId }) => {
     const fetchSubcategories = async () => {
       try {
         const response = await axios.get(`https://portfoolio-deploy-7b060a1c6acd.herokuapp.com/subcategories/category/${categoryId}`);
-        setSubcategories(response.data);
+        console.log('Response data:', response.data); // Отладочная информация
+        // Убедитесь, что response.data является массивом
+        if (Array.isArray(response.data)) {
+          setSubcategories(response.data);
+        } else {
+          console.error('Error: Response data is not an array', response.data);
+          setSubcategories([]);
+        }
       } catch (error) {
         console.error('Error fetching subcategories:', error);
+        setSubcategories([]); // Установите пустой массив в случае ошибки
       }
     };
 
     fetchSubcategories();
   }, [categoryId]);
 
+  // Дополнительные проверки для отладки
+  if (!Array.isArray(subcategories)) {
+    console.error('Subcategories is not an array:', subcategories);
+    return <div>Error: Subcategories data is invalid.</div>;
+  }
+
   return (
-    <Tabs style={{marginTop:'50px'}} defaultActiveKey="all" id="subcategory-tabs">
+    <Tabs style={{ marginTop: '50px' }} defaultActiveKey="all" id="subcategory-tabs">
       <Tab eventKey="all" title="Все работы">
         <ProjectList categoryId={categoryId} subcategoryId="all" />
       </Tab>
