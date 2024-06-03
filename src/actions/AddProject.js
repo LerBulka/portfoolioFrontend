addb34f4470c5aaf107d72939f28ad9987eb4a24
Valeria Ze <valerijazhe@gmail.com>
2024-06-03 21:50:52 +0300

    image

--- src/actions/AddProject.js
+++ src/actions/AddProject.js
@@ -60,7 +60,7 @@ export default function AddProject() {
     
             let formData = new FormData();
             formData.append('file', image.data, image.data.name);
-            await fetch(`https://portfoolio-deploy-7b060a1c6acd.herokuapp.com/image`, {
+            await fetch('http://localhost:5000/image', {
                 method: 'POST',
                 body: formData,
             });
